541da0046657989d45e13fe0eefe0b6d1d76fd15
pclbusto <pclbusto@gmail.com>
2018-07-02 19:23:01 -0300

    animacion de parado

--- testSprites.py
+++ testSprites.py
@@ -32,6 +32,15 @@ VERDE = (0, 255, 0)
 ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
 VIOLETA = (98, 0, 255)
+
+STATE_PARADO = 0
+STATE_CORRIENDO = 1
+STATE_SALTANDO_UP = 2
+STATE_SALTANDO_DOWN = 3
+DIRECCION_IZQUIERDA = -1
+DIRECCION_DERECHA = 1
+
+
 FPS = 60
 pygame.init()
 
@@ -47,19 +56,30 @@ hecho = False
 # Se usa para esta blecer cuan rápido se actualiza la pantalla
 
 megaman = pygame.image.load("Megaman/sprites megaman.png").convert_alpha()
-fondo = pygame.image.load("fondo.png")
+# fondo = pygame.image.load("fondo.png")
+list_right_stand = []
+list_left_stand = []
 list_right_run = []
 list_left_run = []
 list_jump_right = []
 list_jump_left = []
 porcentaje_salto = []
-for x in range(0,90,5):
+for x in range(0,90,4):
     porcentaje_salto.append(math.sin(math.radians(x)))
     print(math.sin(math.radians(x)))
+longitud_lista_porcentaje =len(porcentaje_salto)-2
 print(len(porcentaje_salto))
 
+addImage(list_left_stand, megaman,4,19,23,41,True)
+addImage(list_left_stand, megaman,29,19,48,41,True)
+addImage(list_left_stand, megaman,54,19,73,41,True)
+
+addImage(list_right_stand, megaman,4,19,23,41,False)
+addImage(list_right_stand, megaman,29,19,48,41,False)
+addImage(list_right_stand, megaman,54,19,73,41,False)
+
 # addImage(list_right_run,megaman,2,19,25,41, True)
-addImage(list_right_run,megaman,81,18,104,41, True)
+addImage(list_right_run ,megaman,81,18,104,41, True)
 addImage(list_right_run,megaman,107,18,130,41, True)
 addImage(list_right_run,megaman,135,18,158,41, True)
 addImage(list_right_run,megaman,107,18,130,41, True)
@@ -100,33 +120,37 @@ while not hecho:
     if lista[pygame.K_LEFT]==1:
         index = int(i) % len(list_left_run)
         i -= delta
-        direccion = -1
+        direccion = DIRECCION_IZQUIERDA
+        estado = STATE_CORRIENDO
 
     if lista[pygame.K_RIGHT] == 1:
         index = int(i) % len(list_left_run)
         i += delta
-        direccion = 1
+        direccion = DIRECCION_DERECHA
+    if lista[pygame.K_RIGHT] == 0:
+        estado =STATE_PARADO
     if lista[pygame.K_a]==1:
         if estado == 0:
-            estado=1
+            estado=STATE_SALTANDO_UP
             index_porcentaje_salto += 1
-        elif estado == 1 and index_porcentaje_salto<16:
+        elif estado == STATE_SALTANDO_UP and index_porcentaje_salto<longitud_lista_porcentaje:
             index_porcentaje_salto +=1
-        elif estado == 1 and index_porcentaje_salto>=16:
-            estado = 2
+        elif estado == STATE_SALTANDO_UP and index_porcentaje_salto>=longitud_lista_porcentaje:
+            estado = STATE_SALTANDO_DOWN
             index_porcentaje_salto -= 1
-        elif estado == 2 and index_porcentaje_salto > 0:
+        elif estado == STATE_SALTANDO_DOWN and index_porcentaje_salto > 0:
             index_porcentaje_salto -= 1
-        elif estado == 2 and index_porcentaje_salto == 0:
+        elif estado == STATE_SALTANDO_DOWN and index_porcentaje_salto == 0:
             index = 0
-            estado = 0
-        print("index: {} estado {} pos salto {}".format(index_porcentaje_salto,estado,150-(80* porcentaje_salto[index_porcentaje_salto])))
+            estado = STATE_PARADO
+        # print("index: {} estado {} pos salto {}".format(index_porcentaje_salto,estado,150-(80* porcentaje_salto[index_porcentaje_salto])))
 
     if lista[pygame.K_a]==0 and estado in[1,2]:
         if index_porcentaje_salto >0:
             index_porcentaje_salto -= 1
+            estado = STATE_SALTANDO_DOWN
         elif index_porcentaje_salto == 0:
-            estado = 0
+            estado = STATE_PARADO
 
 
     # --- LA LÓGICA DEL JUEGO DEBERÍA IR AQUÍ
@@ -135,19 +159,23 @@ while not hecho:
 
     # Primero, limpia la pantalla con blanco. No vayas a poner otros comandos de dibujo encima 
     # de esto, de otra forma serán borrados por este comando:
-
-    # pantalla.fill(BLANCO)
-    pantalla.blit(fondo,(0,0))
+    pantalla.fill(NEGRO)
+    # pantalla.blit(fondo,(0,0))
     # index = int(i)%len(list_left_run)
-    if direccion == 1 and estado == 0:
+    if direccion == DIRECCION_DERECHA and estado == STATE_CORRIENDO:
         pantalla.blit(list_right_run[index], (i*speed, 150))
-    elif direccion == -1 and estado == 0:
+    elif direccion == DIRECCION_IZQUIERDA and estado == STATE_CORRIENDO:
         pantalla.blit(list_left_run[index], (i *speed , 150))
-    elif direccion == 1 and estado in[1,2]:
-        pantalla.blit(list_jump_right[0], (i * speed, 150-(80* porcentaje_salto[index_porcentaje_salto])))
-    elif direccion == -1 and estado in[1,2]:
-        pantalla.blit(list_jump_left[0], (i * speed, 150-(80*porcentaje_salto[index_porcentaje_salto])))
-
+    elif direccion == DIRECCION_DERECHA and estado in[STATE_SALTANDO_UP,STATE_SALTANDO_DOWN]:
+        pantalla.blit(list_jump_right[0], (i * speed, 150-(110* porcentaje_salto[index_porcentaje_salto])))
+    elif direccion == DIRECCION_IZQUIERDA and estado in[STATE_SALTANDO_UP,STATE_SALTANDO_DOWN]:
+        pantalla.blit(list_jump_left[0], (i * speed, 150-(110*porcentaje_salto[index_porcentaje_salto])))
+    elif direccion == DIRECCION_IZQUIERDA and estado in[STATE_PARADO]:
+        pantalla.blit(list_left_stand[0], (i , 150))
+    elif direccion == DIRECCION_DERECHA and estado ==STATE_PARADO:
+        pantalla.blit(list_right_stand[0], (i , 150))
+    elif direccion == DIRECCION_IZQUIERDA and estado ==STATE_PARADO:
+        pantalla.blit(list_left_stand[0], (i , 150))
     # pantalla.blit(list_run[6], (0, 0))
 
     # --- Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
