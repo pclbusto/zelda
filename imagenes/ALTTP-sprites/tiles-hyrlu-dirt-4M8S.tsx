ddbfb2d95e44bf7b4ff8f1c4ff39a66b02df2d1e
pedro cesar luis busto <pclbusto@gmail.com>
2024-10-07 07:02:31 -0300

    limpieza y organizacion

---
+++ imagenes/ALTTP-sprites/tiles-hyrlu-dirt-4M8S.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles-hyrlu-dirt" tilewidth="64" tileheight="64" spacing="8" margin="4" tilecount="5194" columns="53">
+ <image source="tiles-hyrlu-dirt.png" width="3864" height="7092"/>
+</tileset>
